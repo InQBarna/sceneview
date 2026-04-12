ad7bf9510c44e7fa853e80d53e3346d98b356296
David García <david.garcia@inqbarna.com>
2026-04-12 12:22:28 +0200

    Build Adapted to Jitpack

--- sceneview-core/src/androidMain/kotlin/io/github/sceneview/logging/Log.kt
+++ sceneview-core/src/androidMain/kotlin/io/github/sceneview/logging/Log.kt
@@ -1,8 +1,24 @@
 package io.github.sceneview.logging
 
+import java.util.logging.ConsoleHandler
 import java.util.logging.Level
 import java.util.logging.Logger
 
+
+private object AndroidLog {
+    init {
+        Logger.getLogger("").apply {
+            addHandler(ConsoleHandler())
+            level = Level.FINEST
+        }
+    }
+
+    fun logWarning(tag: String, message: String) {
+        Logger.getLogger(tag).log(Level.INFO, message)
+    }
+}
+
+
 actual fun logWarning(tag: String, message: String) {
-    Logger.getLogger(tag).log(Level.WARNING, message)
+    AndroidLog.logWarning(tag, message)
 }
